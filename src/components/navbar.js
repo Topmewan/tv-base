2f59d4664c983a9a1336b81f8874edb3e32ba156
Topmewan <anon8733@gmail.com>
2021-10-03 20:12:39 +0300

    style navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,25 @@
-
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return(
-        <div className="">
-            This is nav
+        <div className="navbar">
+            <div className="container">
+                <nav className="navbar__nav">
+                    <h3 className="nav__brand">
+                        <Link  to="/">
+                            <i className='fas fa-video'></i> TV Show Base
+                        </Link>
+                    </h3>
+                    <ul className="nav__links">
+                        <li className="link__link">
+                            <Link to = '/'>Home</Link>
+                        </li>
+                        <li className="link__link">
+                            <Link to = '/about'>About</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
