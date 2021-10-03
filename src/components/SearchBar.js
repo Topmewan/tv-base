67d28febbb0edf71b7bccc05459ade919e320f93
Topmewan <anon8733@gmail.com>
2021-10-03 20:27:15 +0300

    test

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,31 @@
+import {useState} from "react";
 
 
 function SearchBar() {
+    const [search,setSearch] = useState("");
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(search)
+
+
+    }
     return (
-        <div className="">gfgf</div>
+        <div className="searchbar">
+            <form className="searchbar__form">
+                <input
+                    type="text"
+                    placeholder="Search for TV Show"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                    className="btn btn-block"
+                    onClick={handleSearch}>
+                    Search</button>
+            </form>
+
+        </div>
 
 
     );
