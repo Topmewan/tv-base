b04b80aaa24f3522976eccd0b0a61c1607f3fba9
Topmewan <anon8733@gmail.com>
2021-10-04 22:11:00 +0300

    render items into new comp - listitem

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import SearchBar from "../components/SearchBar";
 import {useContext} from "react";
 import ShowsContext from '../context/shows/showsContext';
+import ListItem from "../components/ListItem";
 
 const HomePage = () => {
     const showContext = useContext(ShowsContext);
@@ -10,7 +11,14 @@ const HomePage = () => {
             <SearchBar/>
             {loading ? <h2>Loading..</h2> :
                 <div>
-                    {shows.map(item => (<h3>{item.show.name}</h3>))}
+                    {shows.map(item => <ListItem
+                        key={item.show.id}
+                        id={item.show.id}
+                        image={item.show.image ? item.show.image.medium : "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"}
+                        name={item.show.name}
+                        rating={item.show.rating.average ? item.show.rating.average : "No rating"}
+                    />
+                    )}
                 </div>
             }
         </div>
